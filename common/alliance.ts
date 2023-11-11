03af10f6e5f1554b35b832c894172ad599610b41
O.S <owen77stubbs@gmail.com>
2023-11-11 17:37:23 +0000

    Update alliance.ts

--- common/alliance.ts
+++ common/alliance.ts
@@ -1,12 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
 import { BaseHelper } from "./base.js"
 
 import type { Client } from "discord.js"
 import { Colors } from "discord.js"
 
+import { getAlliances } from "../bot/utils/aurora.js"
+import { type Alliance } from "../bot/types.js"
+
 class AllianceHelper extends BaseHelper {
+    data: any
+
+    dbAlliances: Alliance[]
+
     constructor(client: Client, isNova = false) {
         super(client, isNova)
         this.embed.setColor(Colors.DarkBlue)
+    }
+
+    addField(name: string, value: string, inline = false) {
+        this.embed.addFields({ name, value, inline })
+        return this.embed
+    }
+
+    async init(input: string) {
+        this.dbAlliances = await getAlliances()
+    }
+    
+    async lookup(name: string) {
+
     }
 }
 
